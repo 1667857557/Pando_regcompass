3ff3dc0a9687a4d13f4f3b66941692cc92c7492b
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-31 16:30:07 +0800

    Map sparse inputs for native product kernel

--- src/condition_product.cpp
+++ src/condition_product.cpp
@@ -1,4 +1,7 @@
 #include <RcppEigen.h>
+#include "condition_product_core.h"
+
+using Eigen::SparseMatrix;
 
 // [[Rcpp::export]]
 SEXP condition_product_matrix_cpp(
@@ -7,5 +10,23 @@ SEXP condition_product_matrix_cpp(
     Rcpp::IntegerVector left_index,
     Rcpp::IntegerVector right_index
 ) {
-    return R_NilValue;
+    Eigen::MappedSparseMatrix<double> left_mapped =
+        Rcpp::as<Eigen::MappedSparseMatrix<double>>(left_matrix);
+    Eigen::MappedSparseMatrix<double> right_mapped =
+        Rcpp::as<Eigen::MappedSparseMatrix<double>>(right_matrix);
+    SparseMatrix<double> left(left_mapped);
+    SparseMatrix<double> right(right_mapped);
+    left.makeCompressed();
+    right.makeCompressed();
+
+    if (left.rows() != right.rows()) {
+        Rcpp::stop("Input matrices must have identical row counts.");
+    }
+    if (left_index.size() != right_index.size()) {
+        Rcpp::stop("Column index vectors must have equal lengths.");
+    }
+    SparseMatrix<double> answer = condition_product_core(
+        left, right, left_index, right_index
+    );
+    return Rcpp::wrap(answer);
 }
