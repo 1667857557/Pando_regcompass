7a8c7e8df6d081ace2ad16b012e0d9d1e8fe8497
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-08-03 20:07:01 +0800

    Validate BPPARAM on every condition GRN path

--- R/zzzz_grn_runtime_contract.R
+++ R/zzzz_grn_runtime_contract.R
@@ -53,6 +53,57 @@ infer_grn.GRNData <- function(
     ), list(...)))
 }
 
+.pando_infer_condition_grn_routed_impl <- infer_condition_grn.GRNData
+
+infer_condition_grn.GRNData <- function(
+    object, cell_type_col = NULL, condition_col = NULL, cell_type = NULL,
+    genes = NULL, network_name = "condition_grn",
+    peak_to_gene_method = c("Signac", "GREAT"), upstream = 100000,
+    downstream = 0, extend = 1000000, only_tss = FALSE,
+    peak_to_gene_domains = NULL, rna_layer = "data", peak_layer = "data",
+    peak_value_type = c("normalized", "probability", "other"),
+    tf_cor = 0.1, peak_cor = 0,
+    min_cells_per_condition = 50L,
+    small_condition_action = c("error", "drop_condition", "skip_cell_type"),
+    adjust_method = "BH", padj_threshold = 0.05,
+    rank_action = c("mark", "error"), min_residual_df = 1L,
+    parallel = FALSE, BPPARAM = NULL,
+    parallel_scope = c("auto", "cell_type", "target"),
+    overwrite = FALSE, fallback_args = list(), verbose = TRUE, ...) {
+    .pando_validate_bpparam(BPPARAM)
+    do.call(.pando_infer_condition_grn_routed_impl, c(list(
+        object = object,
+        cell_type_col = cell_type_col,
+        condition_col = condition_col,
+        cell_type = cell_type,
+        genes = genes,
+        network_name = network_name,
+        peak_to_gene_method = peak_to_gene_method,
+        upstream = upstream,
+        downstream = downstream,
+        extend = extend,
+        only_tss = only_tss,
+        peak_to_gene_domains = peak_to_gene_domains,
+        rna_layer = rna_layer,
+        peak_layer = peak_layer,
+        peak_value_type = peak_value_type,
+        tf_cor = tf_cor,
+        peak_cor = peak_cor,
+        min_cells_per_condition = min_cells_per_condition,
+        small_condition_action = small_condition_action,
+        adjust_method = adjust_method,
+        padj_threshold = padj_threshold,
+        rank_action = rank_action,
+        min_residual_df = min_residual_df,
+        parallel = parallel,
+        BPPARAM = BPPARAM,
+        parallel_scope = parallel_scope,
+        overwrite = overwrite,
+        fallback_args = fallback_args,
+        verbose = verbose
+    ), list(...)))
+}
+
 .pando_merge_cell_type_grn_results_impl <-
     .pando_merge_cell_type_grn_results
 
