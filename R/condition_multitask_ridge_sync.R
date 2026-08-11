83e392601cec0916641ecc37bf1986aabcef347d
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-08-11 12:26:32 +0800

    Synchronize screened fit dictionary into Pando networks

---
+++ R/condition_multitask_ridge_sync.R
@@ -0,0 +1,25 @@
+# Keep each stored Pando Network synchronized with the screened ConditionGRNFit.
+# This file loads after condition_multitask_ridge_significance.R.
+
+.condition_update_network_significance <- function(object, fit) {
+    for (condition in fit$condition_levels) {
+        network_name <- fit$network_names[[condition]]
+        network <- object@grn@networks[[network_name]]
+        if (is.null(network)) next
+        coefs_one <- fit$coefficients[
+            as.character(fit$coefficients$condition) == condition,
+            , drop = FALSE
+        ]
+        methods::slot(network, "coefs") <- coefs_one
+        params <- methods::slot(network, "params")
+        params$edge_dictionary <- fit$edge_dictionary
+        params$padj_threshold <- fit$padj_threshold
+        params$projection_policy <- .condition_significant_projection_policy
+        params$fit_dictionary_policy <- .condition_fit_dictionary_policy
+        params$candidate_edge_count <- fit$candidate_edge_count
+        params$fit_dictionary_edge_count <- fit$fit_dictionary_edge_count
+        methods::slot(network, "params") <- params
+        object@grn@networks[[network_name]] <- network
+    }
+    object
+}
