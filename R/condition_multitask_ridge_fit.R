86a43bef376536b5a047793467faa9a42aab10c7
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-08-15 02:57:08 +0800

    Fix ridge target R2 semantics

--- R/condition_multitask_ridge_fit.R
+++ R/condition_multitask_ridge_fit.R
@@ -132,7 +132,7 @@
         fits[[i]] <- data.frame(
             target = edges$target[[1L]],
             condition = condition,
-            rsq = cv$rsq_oof[[i]],
+            rsq = fit$rsq[[i]],
             rsq_oof = cv$rsq_oof[[i]],
             rsq_in_sample = fit$rsq[[i]],
             rank = as.integer(1L + fit$raw_rank[[i]]),
